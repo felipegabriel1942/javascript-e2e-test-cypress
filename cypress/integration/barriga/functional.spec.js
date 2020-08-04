923782b61c0615b3c72038c03424a8f5da1e98dd
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-08-04 18:59:02 -0300

    Finalizados testes funcionais

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -17,6 +17,8 @@ describe('Should test at a functional level', () => {
 
         cy.acessarMenuConta();
 
+        // cy.get(loc.MENU.HOME).click();
+
     });
 
     it('Should create an account', () => {
@@ -29,7 +31,7 @@ describe('Should test at a functional level', () => {
 
     it('Should update an account', () => {
 
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click();
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click();
 
         cy.get(loc.CONTAS.NOME)
             .clear()
@@ -62,6 +64,8 @@ describe('Should test at a functional level', () => {
 
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter');
 
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes');
+
         cy.get(loc.MOVIMENTACAO.STATUS).click();
 
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
@@ -70,7 +74,43 @@ describe('Should test at a functional level', () => {
 
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7);
 
-        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist');
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist');
+
+    });
+
+    it('Should get balance', () => {
+
+        cy.get(loc.MENU.HOME).click();
+
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00' );
+
+        cy.get(loc.MENU.EXTRATO).click();
+
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click();
+
+        // cy.wait(1000);
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo');
+
+        cy.get(loc.MOVIMENTACAO.STATUS).click();
+
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
+
+        cy.get(loc.MESSAGE).should('contain', 'sucesso');
+
+        cy.get(loc.MENU.HOME).click();
+
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00' );
+
+    });
+
+    it('Should remove a transaction', () => {
+
+        cy.get(loc.MENU.EXTRATO).click();
+
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click();
+
+        cy.get(loc.MESSAGE).should('contain', 'sucesso');
 
     });
 });
